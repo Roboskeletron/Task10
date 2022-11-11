95cd263016ccbd05f3d011ac6bcdf43fd1e2b2a3
Roboskeletron <wwwrt287@gmail.com>
2022-11-11 09:22:48 +0300

    packages moved, console app implemented

---
+++ src/task10/console/Main.java
@@ -0,0 +1,19 @@
+package task10.console;
+
+import task10.ArrayIO;
+import task10.Triangle;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        FileInputStream inputStream = new FileInputStream(args[0]);
+        FileOutputStream outputStream = new FileOutputStream(args[1]);
+
+        var triangles = ArrayIO.getTriangles(inputStream, ";");
+
+        var table = Triangle.getTable(triangles);
+
+        System.out.println(ArrayIO.saveTable(table, outputStream));
+    }
+}
