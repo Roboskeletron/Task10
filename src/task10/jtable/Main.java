6b53bb76e9e0fa2cedccd41c6f67a1669e518473
Roboskeletron <wwwrt287@gmail.com>
2022-11-13 20:02:15 +0300

    jtable form created

--- src/task10/jtable/Main.java
+++ src/task10/jtable/Main.java
@@ -2,6 +2,6 @@ package task10.jtable;
 
 public class Main {
     public static void main(String[] args){
-
+        java.awt.EventQueue.invokeLater(() -> new MainForm().setVisible(true));
     }
 }
