ba69a256a7fec31b128261be6d12ddae8d71cc1b
Roboskeletron <wwwrt287@gmail.com>
2022-11-13 20:03:17 +0300

    ArrayConvertor created

---
+++ src/task10/jtable/ArrayConvertor.java
@@ -0,0 +1,47 @@
+package task10.jtable;
+
+import task10.Triangle;
+
+import javax.swing.table.DefaultTableModel;
+import java.util.List;
+import java.util.Vector;
+
+public class ArrayConvertor {
+    public static Vector<Vector<Double>> trianglesToRow(List<Triangle> triangles){
+        Vector<Vector<Double>> rows = new Vector<>();
+
+        for (var triangle : triangles){
+            Vector<Double> row = new Vector<>();
+            for (var point : triangle.vertices){
+                row.add(point.x());
+                row.add(point.y());
+            }
+            rows.add(row);
+        }
+
+        return rows;
+    }
+
+    public static String inputTableToString(DefaultTableModel model){
+        StringBuilder builder = new StringBuilder();
+        int count = 0;
+
+        for (var row : model.getDataVector()){
+            for (var coordinate : row){
+                Double val = (Double) coordinate;
+                builder.append(val);
+                count++;
+
+                if (count >= 2)
+                    builder.append("; ");
+                else {
+                    builder.append(" ");
+                    count = 0;
+                }
+            }
+            builder.append("\n");
+        }
+
+        return builder.toString();
+    }
+}
